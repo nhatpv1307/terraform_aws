ab0aee71111f283f647dc26981d0f991d79d2bc0
NhatPv <nhatpv1307@gmail.com>
2022-08-22 10:08:53 +0700

    2st

--- ec2_ebs_webstatic/remote.tf
+++ ec2_ebs_webstatic/remote.tf
@@ -9,9 +9,12 @@ resource "null_resource" "remote" {
   provisioner "remote-exec" {
     inline = [
       "sudo amazon-linux-extras install nginx1 -y",   //Cài đặt nginx
-      "mkdir /dev/sdh/usr/share/nginx/html",      // Add folder 
-      "sudo mv /home/ec2-user/tropiko-html /dev/sdh/usr/share/nginx/html", //Chuyển thư mục /home/ec2-user/tropiko-html vào /dev/sdh/usr/share/nginx/html
-      "sudo sed -i s+/dev/sdh/usr/share/nginx/html+/dev/sdh/usr/share/nginx/html/tropiko-html+g /etc/nginx/nginx.conf", //thay thế đường dẫn root trong nginx.conf
+      "sudo yum install xfsprogs -y",                 // cai goi bo tro
+      "sudo mkfs -t xfs /dev/xvdh",          //format disk
+      "sudo mkdir /data",                   //Tao folder data
+      "sudo mount /dev/xvdh /data",         //Mount disk to data   
+      "sudo mv /home/ec2-user/tropiko-html /data", //Chuyển thư mục /home/ec2-user/tropiko-html vào /dev/sdh/usr/share/nginx/html
+      "sudo sed -i s+/usr/share/nginx/html+/data/tropiko-html+g /etc/nginx/nginx.conf", //thay thế đường dẫn root trong nginx.conf
       "sudo service nginx start" //khởi động service nginx
     ]
   }
