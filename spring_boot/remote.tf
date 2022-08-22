bd9b065cdbb302bee45aa5dae2cc8fa53a503f99
NhatPv <nhatpv1307@gmail.com>
2022-08-22 14:03:17 +0700

    add bai2

--- spring_boot/remote.tf
+++ spring_boot/remote.tf
@@ -11,8 +11,8 @@ resource "null_resource" "remote" {
       "cd /tmp",   //Chuyen qua tmp
       "sudo wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm",                 // tai java 17
       "sudo rpm -Uvh jdk-17_linux-x64_bin.rpm",          //cai dat goi
-      "sudo mkdir /data",
-      "sudo mv /home/ec2-user/jar /data",
+ #     "sudo mkdir /data",                                 // Tao thu muc data
+      "sudo mv /home/ec2-user/jar /data",               //CP file jar qua thu muc
       "sudo java -jar /data/apiserver-1.0.jar",                   //Chay jar
     ]
   }
